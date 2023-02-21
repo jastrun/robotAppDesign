019c05d2bd438d592f70634a3d4093efbbe7a86f
jastrun <1731176506@qq.com>
2023-02-21 17:50:37 +0800

    同步 新建机器人v1

--- widget/dbTree.py
+++ widget/dbTree.py
@@ -8,6 +8,9 @@ from db.sheetOp import *
 class dbTree(QTreeWidget):
     def __init__(self, db):
         super().__init__()
+        self.root = QTreeWidgetItem(self)
+        self.root.setText(0,"机器人数据库")
+        self.robotList=[]
         self.db = db
         self.initTree()
 
@@ -15,14 +18,16 @@ class dbTree(QTreeWidget):
         # 设置树控件
         self.setColumnCount(2)  # 制定树控件为两列
         self.setHeaderLabels(["编码", "名字"])  # 设置列标签
-
         self.expandAll()  # 设置所有的节点为展开的状态
 
     # 同步数据库中的机器人到树
     def TBdb(self):
-        self.clear()
         # 将数据同步到表格中
-        robotSheet=[]
+        root=self.root
+        print(self.invisibleRootItem().removeChild(root))
+        self.root = QTreeWidgetItem(self)
+        self.root.setText(0,"机器人数据库")
+        robotSheet = []
         cur = self.db.cursor()
         sql = "select * from robot"
         cur.execute(sql)
@@ -32,10 +37,10 @@ class dbTree(QTreeWidget):
             print("同步机器人成功！")
         except pymysql.Error as e:
             print("同步机器人失败！" + str(e))
-
+        print(robotSheet)
         for num, name in robotSheet:
             # 添加根节点
-            robot = QTreeWidgetItem(self)
+            robot = QTreeWidgetItem(self.root)
             robot.setText(0, num)
             robot.setText(1, name)
             # 添加运动节点
@@ -51,7 +56,17 @@ class dbTree(QTreeWidget):
             for name in motorSheet:
                 motor = QTreeWidgetItem(robot)
                 motor.setText(0, name[0])
+        self.db.commit()
 
     # 新建机器人
     def creatRobot(self):
-        SixAxisRobot(self.db,'0011')
+        SixAxisRobot(self.db, '0013')
+
+    def deleteRobot(self):
+        # 删除节点
+        item=self.currentItem()
+        for item in self.selectedItems():
+            item.parent().removeChild(item)
+
+
+
